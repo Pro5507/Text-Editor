319044b83fae18f9ed1aa2fcd0fe143853f7145a
Pro5507 <nhupendrakumar9162@gmail.com>
2024-12-26 18:40:22 +0530

    Text Editor

---
+++ te.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from tkinter.filedialog  import askopenfilename, asksaveasfilename
+
+window= Tk()
+window.title("Codingal's Text Editor")
+window.geometry("600x500")
+window.rowconfigure(0, minsize=800, wiegth= 1)
+window.columnconfigure(1, minsize=800, weight=1)
+
+def open_file():
+    filepath= askopenfilename(filetypes=[("Text Files", "*.txt"), ("All files", "*.*")])
+
+    if not filepath:
+        return
+    txt_edit.delete(1.0, END)
+    with open(filepath, "r") as input_file:
+      text= input_file.read()
+      text_edit.insert(END, text)
+      input_file.close()
+    window.title(f"Codingal's Text Editor-{filepath}")
+
+def save_file():
+    filepath= asksaveasfilename(defaultextension="txt", filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+    if not filepath:
+        return
+    with open(filepath, "w") as output_file:
+        text= txt_edit.get(1.0, END)
